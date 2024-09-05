8d5ffb204b38af97f5525f38e8654c2cbdfaa37e
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-09-05 19:01:41 -0300

    CSS - Ejercicio globalizador: Creando estructura header

---
+++ 1_TUP_1_2C_2024/Ruiz_Milton/Ejercicios Javascript/ejercicios.js
@@ -0,0 +1,21 @@
+/* 1. Escribe un programa de una sola línea que haga que aparezca en la pantalla un
+    alert que diga “Hello World”. */
+console.log("¡Hello World!");
+
+/*2. Escribe un programa de una sola línea que escriba en la pantalla un texto que
+diga “Hello World” (document.write).*/
+
+/*3. Escribe un programa de una sola línea que escriba en la pantalla el resultado
+de sumar 3 + 5.
+4. Escribe un programa de dos líneas que pida el nombre del usuario con un
+prompt y escriba un texto que diga “Hola nombreUsuario”
+5. Escribe un programa de tres líneas que pida un número, pida otro número y
+escriba el resultado de sumar estos dos números.
+6. Escribe un programa que pida dos números y escriba en la pantalla cual es el
+mayor.
+7. Escribe un programa que pida 3 números y escriba en la pantalla el mayor de
+los tres.
+8. Escribe un programa que pida un número y diga si es divisible por 2
+9. Escribe un programa que pida una frase y escriba cuantas veces aparece la
+letra a
+10. Escribe un programa que pida una frase y escriba las vocales que aparecen */
